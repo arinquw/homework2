37f06a59ee84971481fd10ef4033188eeacef93e
arinquw <kotoffey@icloud.com>
2025-09-14 20:58:19 +0500

    practice 2

---
+++ practice.py
@@ -0,0 +1,45 @@
+# 1 задание
+
+# a = int(input("Введите первое число: "))
+# b = int(input("Введите второе число: "))
+# c = int(input("Введите третье число: "))
+#
+# summ = a + b + c
+# product = a * b * c
+#
+# print("Сумма чисел:", summ)
+# print("Произведение чисел:", product)
+
+# 2 задание
+
+# gross = float(input("введите зп:"))
+# credit = float(input("введите платеж за кредит:"))
+# home = float(input("введите долг за коммуналку:"))
+
+# summ = gross - credit - home
+
+# print("осталось:", summ)
+
+
+# 3 задание
+
+# d1 = int(input("введите длину 1 диагонали:"))
+# d2 = int(input("введите длину 2 диагонали:"))
+#
+# area = (d1*d2)/2
+#
+# print("площадь ромба:", area)
+
+
+# 4 задание
+
+# print("to be")
+# print("or not")
+# print("to be")
+
+# 5 задание
+
+print("life is what happens")
+print("                       when")
+print("                             you're busy making other plans")
+print("                                                             John Lennon")
